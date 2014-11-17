9da97d90631a5e4bb82fd3ae60aaee053d620083
john gravois <jgravois@esri.com>
2014-11-17 15:10:43 -0500

    upgrade to koop-server 1.4

--- controller/index.js
+++ controller/index.js
@@ -1,72 +1,79 @@
 var citybikes = require('citybikes-js'),
-    extend    = require('node.extend');
+  extend = require('node.extend'),
+  BaseController = require('koop-server/lib/BaseController.js');
 
 // inherit from base controller
-var Controller = extend( {}, BaseController );
-module.exports = Controller;
+var Controller = function(Citybikes) {
 
-// general helper for not found repos
-Controller.notFound = function(req, res){
-  res.send('Must specify a network name with /networks/<networkName> or ask for all networks with /networks', 404);
-};
+  var controller = {};
+  controller.__proto__ = BaseController();
 
-// renders an empty map with a text input 
-Controller.index = function(req, res){
-  res.render(__dirname + '/../views/index');
-};
+  // general helper for not found repos
+  controller.notFound = function(req, res) {
+    res.send('Must specify a network name with /networks/<networkName> or ask for all networks with /networks', 404);
+  };
 
-// general helper for error'd requests
-function sendError(req, res){
-  res.send('There was a problem accessing Citybik.es', 500);
-};
+  // renders an empty map with a text input
+  controller.index = function(req, res) {
+    res.render(__dirname + '/../views/index');
+  };
 
-function getNetworks(req, res, forFeatureServer) {
-  var callback = req.query.callback;
-  delete req.query.callback;
+  // general helper for error'd requests
+  function sendError(req, res) {
+    res.send('There was a problem accessing Citybik.es', 500);
+  };
 
-  Citybikes.findNetworks(req.query, function(err, networks) {
-    if (!err) {
-      if (forFeatureServer !== false) {
-        delete req.query.geometry;
-        Controller._processFeatureServer(req, res, err, networks, callback);
+  function getNetworks(req, res, forFeatureServer) {
+    var callback = req.query.callback;
+    delete req.query.callback;
+    Citybikes.findNetworks(req.query, function(err, networks) {
+      if (!err) {
+        if (forFeatureServer !== false) {
+          delete req.query.geometry;
+          Controller._processFeatureServer(req, res, err, networks, callback);
+        } else {
+          res.json(networks[0]);
+        }
       } else {
-        res.json(networks[0]);
+        sendError(req, res);
       }
-    } else {
-      sendError(req, res);
-    }
-  });
-}
-
-function getStations(req, res, forFeatureServer) {
-  var callback = req.query.callback;
-  delete req.query.callback;
-
-  Citybikes.findStations(req.params.networkName, req.query, function(err, stations) {
-    if (!err) {
-      if (forFeatureServer !== false) {
-        Controller._processFeatureServer(req, res, err, stations, callback);
-      } else {
-        res.json(stations[0]);
-      }
-    } else {
-      sendError(req, res);
-    }
-  });
-}
-
-Controller.networks = function(req, res) {
-  getNetworks(req, res, false);
-}
-
-Controller.stations = function(req, res) {
-  getStations(req, res, false);
-}
-
-Controller.featureservice = function(req, res){
-  if (req.params.networkName) {
-    getStations(req, res);
-  } else {
-    getNetworks(req, res);
+    });
   }
-};
+
+  function getStations(req, res, forFeatureServer) {
+    var callback = req.query.callback;
+    delete req.query.callback;
+
+    Citybikes.findStations(req.params.networkName, req.query, function(err, stations) {
+      if (!err) {
+        if (forFeatureServer !== false) {
+          controller.processFeatureServer(req, res, err, stations, callback);
+        } else {
+          res.json(stations[0]);
+        }
+      } else {
+        sendError(req, res);
+      }
+    });
+  }
+
+  controller.networks = function(req, res) {
+
+    getNetworks(req, res, false);
+  }
+
+  controller.stations = function(req, res) {
+    getStations(req, res, false);
+  }
+
+  controller.featureservice = function(req, res) {
+    if (req.params.networkName) {
+      getStations(req, res);
+    } else {
+      getNetworks(req, res);
+    }
+  };
+  return controller;
+}
+
+module.exports = Controller;
--- models/Citybikes.js
+++ models/Citybikes.js
@@ -1,57 +1,71 @@
-var citybikes = require('citybikes-js'),
-    _ = require('lodash');
+var citybikesJs = require('citybikes-js'),
+  BaseModel = require('koop-server/lib/BaseModel.js');
+_ = require('lodash');
 
-exports.findNetworks = function(options, callback){
-  var key  = 'Networks',
+var Citybikes = function(koop) {
+
+  var citybikes = {};
+  citybikes.__proto__ = BaseModel(koop);
+
+  citybikes.findNetworks = function(options, callback) {
+    var key = 'Networks',
       type = 'Citybikes';
-  
-  Cache.get(type, key, options, function(cacheErr, entry){
-    if (cacheErr){
-      citybikes.networks(function(err, networks) {
-        if (err) {
-          callback(err, null);
-        } else {
-          var output = [{features:_.map(networks, function(n) {
-            n.properties.stationsURL = "/citybikes/network/" + n.id + "/FeatureServer/0"
-            return n;
-          })}];
-          Cache.insert(type, key, output, 0, function(insertErr, success) {
-            if (success) {
-              callback(null, output);
-            } else {
-              callback(insertErr, null);
-            }
-          });
-        }
-      });
-    } else {
-      callback( null, entry );
-    }
-  });
+
+    koop.Cache.get(type, key, options, function(cacheErr, entry) {
+      if (cacheErr) {
+        citybikesJs.networks(function(err, networks) {
+          if (err) {
+            callback(err, null);
+          } else {
+            var output = [{
+              features: _.map(networks, function(n) {
+                n.properties.stationsURL = "/citybikes/network/" + n.id + "/FeatureServer/0"
+                return n;
+              })
+            }];
+            koop.Cache.insert(type, key, output, 0, function(insertErr, success) {
+              if (success) {
+                callback(null, output);
+              } else {
+                callback(insertErr, null);
+              }
+            });
+          }
+        });
+      } else {
+        callback(null, entry);
+      }
+    });
+  };
+
+  citybikes.findStations = function(networkName, options, callback) {
+    var key = 'Stations_' + networkName,
+      type = 'Citybikes';
+
+    koop.Cache.get(type, key, options, function(cacheErr, entry) {
+      if (cacheErr) {
+        citybikesJs.stations(networkName, function(err, stations) {
+          if (err) {
+            callback(err, null);
+          } else {
+            var output = [{
+              features: stations
+            }];
+            koop.Cache.insert(type, key, output, 0, function(insertErr, success) {
+              if (success) {
+                callback(null, output);
+              } else {
+                callback(insertErr, null);
+              }
+            });
+          }
+        });
+      } else {
+        callback(null, entry);
+      }
+    });
+  };
+  return citybikes;
 };
 
-exports.findStations = function(networkName, options, callback){
-  var key  = 'Stations_' + networkName,
-      type = 'Citybikes';
-  
-  Cache.get(type, key, options, function(cacheErr, entry){
-    if (cacheErr){
-      citybikes.stations(networkName, function(err, stations) {
-        if (err) {
-          callback(err, null);
-        } else {
-          var output = [{features:stations}];
-          Cache.insert(type, key, output, 0, function(insertErr, success) {
-            if (success) {
-              callback(null, output);
-            } else {
-              callback(insertErr, null);
-            }
-          });
-        }
-      });
-    } else {
-      callback( null, entry );
-    }
-  });
-};
+module.exports = Citybikes;
